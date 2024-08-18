890e90c9c97bba94d544e51611a5883d5b7ab559
CrimsonEpitaph42 <daniel.bar.sela10@gmail.com>
2024-08-18 02:12:36 -0400

    Changed emotion_detection.py to return a dict of all scores

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,11 +1,35 @@
 '''Module for detecting emotion in text using the Watson NLP library'''
 
 import requests
+import json
 
 WATSON_URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
 WATSON_HEADERS = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+WATSON_PREDICTION_KEY = 'emotionPredictions'
+EMOTIONS_KEY = 'emotion'
 
 def emotion_detector(text_to_analyze):
     text_json = {"raw_document": {"text" : text_to_analyze}}
     res = requests.post(url=WATSON_URL, json=text_json, headers=WATSON_HEADERS)
-    return res.text
+    res_json = json.loads(res.text)
+    res_emotions = res_json[WATSON_PREDICTION_KEY][0][EMOTIONS_KEY]
+
+    dominant_emotion = ""
+    max_score = 0
+    for emotion in res_emotions.keys():
+        if res_emotions[emotion] > max_score:
+            max_score = res_emotions[emotion]
+            dominant_emotion = emotion
+
+    ret = {
+        'anger': res_emotions['anger'],
+        'disgust': res_emotions['disgust'],
+        'fear': res_emotions['fear'],
+        'joy': res_emotions['joy'],
+        'sadness': res_emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    return ret
+    
+if __name__ == "__main__":
+    print(emotion_detector("I love this tech"))
